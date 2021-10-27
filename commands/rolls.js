d056864f166767f70de77882b0f4dd182711fa87
RikkaHasCookies <imhunter_@outlook.com>
2021-10-27 02:19:41 -0700

    adaptación de código y muestra cantidad de waifus

--- commands/rolls.js
+++ commands/rolls.js
@@ -1,36 +1,26 @@
 const Discord = require('discord.js');
 
 // Importar cosas locales
-const userController = require('../controller/user.controller');
-
-// Modelos
-const User = require('../models/user');
+const { USER_GET, USER_WAIFUS_GET } = require('../controller/user.controller');
 
 module.exports = {
     name: 'rolls',
     category: 'Waifu',
     description: 'Muestra el estado de tus rolls y de reclamación.',
-    async execute (message) {
-        // Buscar usuario
-        let findUser = await User.findOne({ userID: message.author.id, guild: message.guild.id });
-        // Si no se encuentra el usuario
-        if (!findUser) {
-            // Crear perfil
-            findUser = await userController.USER_NEW({
-                id: message.author.id,
-                guild: message.guild.id
-            });
-
-            // Comprobar si se creó el usuario
-            if (!findUser) return message.channel.send({ content: `ocurrió un error al intentar crear tu usuario!` });
-        };
+    aliases: ['roll'],
+    async execute (message, args) {
+        // Comprobar Usuario y Waifus
+        const user = await USER_GET(message);
+        const waifus = await USER_WAIFUS_GET(message.author.id);
+        if (user == false) return message.channel.send({ content: `ocurrió un error al intentar crear tu usuario!` });
 
         // Descripción
-        let description = `Rolls restantes: ${findUser.rolls} 🎲\n`;
+        let description = `Rolls restantes: ${user.rolls} 🎲\n`;
 
         // Comprobación de reclamación
-        if (findUser.canClaim == true) description += `Puedes reclamar: **Sí** ✅`;
-        if (findUser.canClaim == false) description += `Puedes reclamar: **No** ❌`;
+        if (user.canClaim == true) description += `Puedes reclamar: **Sí** ✅`;
+        if (user.canClaim == false) description += `Puedes reclamar: **No** ❌`;
+        description += `\n\nWaifus: ${waifus.length} 💞`;
 
         // Embed
         let embed = new Discord.MessageEmbed()
