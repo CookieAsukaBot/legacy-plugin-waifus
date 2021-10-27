f460d37af492a5ee114ea7f0e51014385f6e58f7
RikkaHasCookies <imhunter_@outlook.com>
2021-10-27 02:26:55 -0700

    se adaptó al controlador (mejor lectura)

--- commands/harem.js
+++ commands/harem.js
@@ -1,42 +1,30 @@
 const Discord = require('discord.js');
 
 // Importar cosas locales
-const userController = require('../controller/user.controller');
-
-// Modelos
-const User = require('../models/user');
-const Waifu = require('../models/waifu');
+const { USER_GET, USER_WAIFUS_GET } = require('../controller/user.controller');
 
 module.exports = {
     name: 'harem',
     category: 'Waifu',
-    description: 'Muestra tu colección | o la de un usuario.', // wip
-    async execute (message, args, bot) {
+    description: 'Muestra tu harem | o la de un usuario.', // wip
+    async execute (message) {
         // Detectar si en args hay un usuario
 
         // Buscar usuario
-        let findUser = await User.findOne({ userID: message.author.id, guild: message.guild.id });
-        // Si no se encuentra el usuario
-        if (!findUser) {
-            // Crear perfil
-            findUser = await userController.USER_NEW({
-                id: message.author.id,
-                guild: message.guild.id
-            });
-
-            // Comprobar si se creó el usuario
-            if (!findUser) return message.channel.send({ content: `ocurrió un error al intentar crear tu usuario!` });
-        };
+        const user = await USER_GET(message);
+        if (user == false) return message.channel.send({ content: `ocurrió un error al intentar crear tu usuario!` });
 
         // Buscar Waifus
-        const findWaifus = await Waifu.find({ userID: message.author.id });
+        const waifus = await USER_WAIFUS_GET(message.author.id);
 
         // Comprobar
-        if (!findWaifus) return message.channel.send({
+        if (!waifus) return message.channel.send({
             content: `<@${message.author.id}>, necesitas reclamar waifus primero!`
         });
 
         // Aquí empieza el desastre...
-        message.reply({ content: `El comando sigue en construcción.` });
+        message.reply({
+            content: `El comando sigue en construcción. ⚠`
+        });
     }
 };
