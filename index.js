cd18cfb8b259e15efe95da29eaeda6dacc8b2fe7
RikkaHasCookies <imhunter_@outlook.com>
2021-10-27 19:18:07 -0700

    actualización: soporte para anilist y personajes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     name: 'Waifus',
-    version: '1.3.2',
+    version: '1.4.0',
     cookiebot: '1.0.0',
     description: '¡Plugin oficial para obtener artes aleatorios y reclamarlos!',
     dependencies: ['booru', 'nekos.life', 'nanoid'],
