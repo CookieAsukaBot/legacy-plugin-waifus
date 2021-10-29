bd9668b7f02133e7379c220b5898c3be1cdd27c8
RikkaHasCookies <imhunter_@outlook.com>
2021-10-28 21:56:29 -0700

    cuánto tiempo falta

---
+++ helpers/time-things.js
@@ -0,0 +1,23 @@
+const moment = require('moment');
+moment.locale('es');
+
+const GET_COUNTDOWN_TIME = (time) => {
+    const eventTime = time.unix();
+    const currentTime = moment().unix();
+    const diffTime = eventTime - currentTime;
+    const duration = moment.duration(diffTime * 1000, 'milliseconds');
+
+    // Resultado
+    let timeLeft = `en `;
+    if (duration.minutes() == 1) timeLeft += `${duration.minutes()} minuto `;
+    if (duration.minutes() > 1) timeLeft += `${duration.minutes()} minutos `;
+    if (duration.minutes() !== 0) timeLeft += `y `;
+    if (duration.seconds() > 1) timeLeft += `${duration.seconds()} segundos`;
+    if (duration.seconds() == 1) timeLeft += `${duration.seconds()} segundo`;
+
+    return timeLeft;
+};
+
+module.exports = {
+    GET_COUNTDOWN_TIME
+};
