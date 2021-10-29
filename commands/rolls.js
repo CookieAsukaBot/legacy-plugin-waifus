3d308cb21148b53690a41e66b1312fb26854cf42
RikkaHasCookies <imhunter_@outlook.com>
2021-10-29 04:41:42 -0700

    se agregó contador de waifus y artes

--- commands/rolls.js
+++ commands/rolls.js
@@ -1,26 +1,31 @@
 const { MessageEmbed } = require('discord.js');
 
 // Importar cosas locales
-const { USER_GET, USER_WAIFUS_GET } = require('../controller/user.controller');
+const Waifu = require('../models/waifu');
+const { USER_GET } = require('../controller/user.controller');
 
 module.exports = {
     name: 'rolls',
     category: 'Waifu',
     description: 'Muestra el estado de tus rolls y de reclamación.',
     aliases: ['roll'],
-    async execute (message, args) {
+    async execute (message) {
         // Comprobar Usuario y Waifus
         const user = await USER_GET(message);
-        const waifus = await USER_WAIFUS_GET(message.guild.id, message.author.id);
         if (user == false) return message.channel.send({ content: `ocurrió un error al intentar crear tu usuario!` });
+    
+        // Contadores
+        const ARTS = await Waifu.countDocuments({ guild: message.guild.id, userID: message.author.id, type: "ART" });
+        const WAIFUS = await Waifu.countDocuments({ guild: message.guild.id, userID: message.author.id, type: "WAIFU" });
 
         // Descripción
-        let description = `Rolls restantes: ${user.rolls} 🎲\n`;
+        let description = `Rolls restantes: **${user.rolls}** 🎲\n`;
 
         // Comprobación de reclamación
         if (user.canClaim == true) description += `Puedes reclamar: **Sí** ✅`;
         if (user.canClaim == false) description += `Puedes reclamar: **No** ❌`;
-        description += `\n\nWaifus: ${waifus.length} 💞`;
+        description += `\n\nWaifus: **${WAIFUS}** 💞`;
+        description += `\nArtes: **${ARTS}** 🎨`;
 
         // Embed
         let embed = new MessageEmbed()
