c326641bc8511d579b98ea331ee8e238b7f5deec
RikkaHasCookies <imhunter_@outlook.com>
2021-11-01 17:38:39 -0700

    change version value

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const { Collection } = require('discord.js');
 const path = require('path');
 
 module.exports = {
     name: 'Waifus',
-    version: '1.5.0',
+    version: '1.5.1',
     cookiebot: '1.0.0',
     description: '¡Plugin oficial para obtener artes aleatorios y reclamarlos!',
     dependencies: ['booru', 'nekos.life', 'nanoid'],
