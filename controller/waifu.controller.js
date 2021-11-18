471972d3a9d8a66147abeaf732237774d808f248
RikkaHasCookies <imhunter_@outlook.com>
2021-11-18 10:34:03 -0800

    se remplazó safebooru por danbooru

--- controller/waifu.controller.js
+++ controller/waifu.controller.js
@@ -1,7 +1,5 @@
 const { nanoid } = require('nanoid');
 const Booru = require('booru');
-// const Client = require('nekos.life');
-// const neko = new Client();
 
 const { GENERATE_RANDOM_NUMBER } = require('../helpers/random-things');
 const { ANILIST_RANDOM_CHARACTER } = require('../helpers/api-anilist');
@@ -20,6 +18,60 @@ const RANDOM_ART_SAFEBOORU = async (guild) => {
 
         // Petición
         let res = await Booru.search('safebooru', [], query);
+
+        // Modelo
+        let image = {
+            id: res.posts[0].id,
+            domain: res.posts[0].booru.domain,
+            url: res.posts[0].fileUrl,
+            tags: res.posts[0].tags,
+            description: '',
+            rating: res.posts[0].rating,
+            source: res.posts[0].source,
+            createdAt: res.posts[0].createdAt,
+            type: "ART",
+            owner: false
+        };
+
+        // Comprobar si está reclamada
+        const isClaimed = await WAIFU_SEARCH_BY_ID_AND_DOMAIN(guild, image.id, image.domain);
+        if (isClaimed !== false) image.owner = isClaimed;
+
+        // Comprobar campos
+        // Agregar tags
+        if (image.tags != undefined) image.description += `${image.tags.slice(0, 8).join(' ')}`;
+
+        // Agregar fuentes
+        // Si es un Array
+        if (image.source && Array.isArray(image.source)) {
+            image.description += image.source.forEach((source, index) =>
+                `\n[Fuente (${index + 1}/${image.source.length})](${source})`
+            );
+        };
+        // Si es un String
+        if (image.source && !Array.isArray(image.source)) {
+            image.description += `\n[Fuente](${image.source})`;
+        };
+
+        // Devolver
+        return image;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+};
+
+const RANDOM_ART_DANBOORU = async (guild) => {
+    try {
+        // Query
+        let query = {
+            limit: 1,
+            random: true,
+            showUnavailable: true
+        };
+
+        // Petición
+        let res = await Booru.search('danbooru', ['rating:safe -video'], query);
 
         // Modelo
         let image = {
@@ -162,7 +214,7 @@ const WAIFU_GET_RANDOM = async (guild) => {
 
         switch (true) {
             case (SEED > 49):
-                image = await RANDOM_ART_SAFEBOORU(guild);
+                image = await RANDOM_ART_DANBOORU(guild);
                 break;
             case (SEED > 1 && SEED <= 49):
                 image = await RANDOM_WAIFU_ANILIST(guild);
