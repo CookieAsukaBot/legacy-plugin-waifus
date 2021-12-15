fac3d4d412cd9e786a3f56cb75806f5fe0a768b1
RikkaHasCookies <imhunter_@outlook.com>
2021-12-15 05:16:34 -0800

    1.6.6

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     name: 'Waifus',
-    version: '1.6.5',
+    version: '1.6.6',
     cookiebot: '1.0.0',
     description: '¡Plugin oficial para obtener artes aleatorios y reclamarlos!',
     dependencies: ['booru', 'nanoid', 'canvas'],
